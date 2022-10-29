59df6f5cfdb87edb26ee0b2f953e9c1479b06ae7
kou12345 <hi.0412.ko.2002@gmail.com>
2022-10-29 22:32:09 +0900

    フォルダ構成を変更

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9-buster
 
 WORKDIR /workspace
 
-COPY requirements.txt .
+COPY /app/requirements.txt .
 
 # pip install を記載
 RUN pip install -U pip \
---
+++ app/main.py
@@ -0,0 +1,10 @@
+from symbol import import_from
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_root():
+    return {"hello": "world"}
