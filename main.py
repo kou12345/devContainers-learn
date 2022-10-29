954d4836dca742ec2791ffc26aa366ace495cdd4
kou12345 <hi.0412.ko.2002@gmail.com>
2022-10-29 02:50:01 +0000

    fastapiの動作確認を追記

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Dev Containers から hello")
+from symbol import import_from
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_root():
+    return {"hello": "container"}
